9841176b9288637e0389680de8f60c6a02de8a93
jims-p <jim@neoinformatics.com>
2015-01-09 17:56:36 -0500

    mainpage first structure

---
+++ index.php
@@ -0,0 +1,104 @@
+<?php 
+  define('PAGE','home')
+?><!doctype html><!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>1001 rue Lenoir, Montréal</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" type="text/css" href="css/screen.css">
+
+    <link rel="stylesheet" href="//brick.a.ssl.fastly.net/Raleway:200,400/Open+Sans:400,700">
+    <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+
+
+      <div class="header row">
+        <div class="large-3 columns">
+          <a class="1mile_logo" href="#"><img src=""></a>
+          <a class="rca_logo" href="#"><img src=""></a>
+        </div>
+        <div class="large-6 columns title">
+          <div>
+            <h1>1001, rue Lenoir, Montréal</h1>
+            <span>Text here</span>
+          </div>
+        </div>
+        <div class="large-3 columns navig">
+          <a class="info_btn" href="#"><img src="img/icon_info.png"></a>
+        </div>
+      </div>
+      
+       
+      
+      
+      <div class="row">    
+        
+         
+         
+        <div class="large-10 columns">
+          <div class="row">
+            <div class="topbanner">
+              <img src="img/adbanner_728x90.png" alt="Ad banner">
+            </div>
+          </div>
+
+          <div class="row content">
+            <div class="large-5 columns leftcontent">
+              <div class="row">
+                <img src="img/temp_caroussel.jpg">
+              </div>
+
+              <h3>Page Title <small>Page subtitle</small></h3>
+            
+              <p>Bacon ipsum dolor sit amet salami ham hock biltong ball tip drumstick sirloin pancetta meatball short loin. Venison tail chuck pork chop, andouille ball tip beef ribs flank boudin bacon. Salami andouille pork belly short ribs flank cow. Salami sirloin turkey kielbasa. Sausage venison pork loin leberkas chuck short loin, cow ham prosciutto pastrami jowl. Ham hock jerky tri-tip, fatback hamburger shoulder swine pancetta ground round. Tri-tip prosciutto meatball turkey, brisket spare ribs shankle chuck cow chicken ham hock boudin meatloaf jowl.</p>
+         
+              <p>Ground round pastrami pork loin tenderloin jerky. Jerky spare ribs biltong, ham hock ham capicola pork. Jerky turducken pork, meatloaf sausage capicola swine corned beef turkey short loin. Tongue prosciutto pork loin, ground round spare ribs venison kielbasa strip steak.</p>
+         
+              <p>Hamburger bresaola turkey t-bone, leberkas salami pork chop ham hock beef ribs. Rump biltong meatball venison, short ribs pork loin shank shankle corned beef beef. Cow salami jowl short loin hamburger fatback. Short ribs pork belly shoulder pastrami drumstick salami corned beef ham hock bresaola. Swine filet mignon cow sausage ball tip. Cow ribeye ground round, sausage pork loin pig beef ball tip turkey boudin.</p>
+         
+              <p>Prosciutto ball tip filet mignon andouille frankfurter chicken rump sausage meatball. Filet mignon meatloaf ground round andouille ham hock pork. Bresaola short loin meatball chuck hamburger pig. Turkey venison chuck, tongue fatback tail swine jerky corned beef shank kielbasa prosciutto ribeye ham tri-tip. Rump bacon pork belly meatloaf shoulder short loin meatball kielbasa pork loin tongue bresaola brisket corned beef jowl prosciutto. Beef ribs shankle short ribs pork belly corned beef fatback pork chop tongue biltong boudin strip steak sirloin meatloaf pancetta.</p>
+
+            </div>
+
+            <div class="large-7 columns rightcontent">
+              <h3>Page Title <small>Page subtitle</small></h3>
+            
+              <p>Bacon ipsum dolor sit amet salami ham hock biltong ball tip drumstick sirloin pancetta meatball short loin. Venison tail chuck pork chop, andouille ball tip beef ribs flank boudin bacon. Salami andouille pork belly short ribs flank cow. Salami sirloin turkey kielbasa. Sausage venison pork loin leberkas chuck short loin, cow ham prosciutto pastrami jowl. Ham hock jerky tri-tip, fatback hamburger shoulder swine pancetta ground round. Tri-tip prosciutto meatball turkey, brisket spare ribs shankle chuck cow chicken ham hock boudin meatloaf jowl.</p>
+         
+              <p>Ground round pastrami pork loin tenderloin jerky. Jerky spare ribs biltong, ham hock ham capicola pork. Jerky turducken pork, meatloaf sausage capicola swine corned beef turkey short loin. Tongue prosciutto pork loin, ground round spare ribs venison kielbasa strip steak.</p>
+         
+              <p>Hamburger bresaola turkey t-bone, leberkas salami pork chop ham hock beef ribs. Rump biltong meatball venison, short ribs pork loin shank shankle corned beef beef. Cow salami jowl short loin hamburger fatback. Short ribs pork belly shoulder pastrami drumstick salami corned beef ham hock bresaola. Swine filet mignon cow sausage ball tip. Cow ribeye ground round, sausage pork loin pig beef ball tip turkey boudin.</p>
+         
+              <p>Prosciutto ball tip filet mignon andouille frankfurter chicken rump sausage meatball. Filet mignon meatloaf ground round andouille ham hock pork. Bresaola short loin meatball chuck hamburger pig. Turkey venison chuck, tongue fatback tail swine jerky corned beef shank kielbasa prosciutto ribeye ham tri-tip. Rump bacon pork belly meatloaf shoulder short loin meatball kielbasa pork loin tongue bresaola brisket corned beef jowl prosciutto. Beef ribs shankle short ribs pork belly corned beef fatback pork chop tongue biltong boudin strip steak sirloin meatloaf pancetta.</p>
+            </div>
+          </div>
+          
+                
+        </div>
+        
+        
+         
+         
+        <div class="large-2 columns sidepanel">
+          <div class="toppanel">
+            <a class="button" href="#">Classifieds</a>
+          </div>
+          
+          <div class="secondbanner">
+            <p><img src="img/adbanner_300x600.png"/></p>
+          </div>
+            
+        </div>
+        
+      </div>
+        
+      
+       
+      
+   
+  </body>
+</html>
