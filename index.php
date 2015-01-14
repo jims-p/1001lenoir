8d46047d2b6376559093f6bf5312ae870dc7cd47
jims-p <jim@neoinformatics.com>
2015-01-14 18:33:16 -0500

    mainpage announcements + list
    to add new member, copy div as indicated and always give "end" as class
    name for last div of the list

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
     <title>1001 rue Lenoir, Montréal</title>
     <link rel="stylesheet" href="css/foundation.css" />
     <link rel="stylesheet" type="text/css" href="css/screen.css">
+    <link rel="stylesheet" href="css/general_foundicons.css">
+    <!--[if lt IE 8]>
+      <link rel="stylesheet" href="css/general_foundicons_ie7.css">
+    <![endif]-->
 
     <link rel="stylesheet" href="//brick.a.ssl.fastly.net/Raleway:200,400/Open+Sans:400,700">
     <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
@@ -16,85 +20,196 @@
   <body>
 
 
-      <div class="header row">
-        <div class="large-3 columns">
+    <div class="header row">
+      <div class="medium-3 large-3 columns">
+        <a class="1mile_logo" href="#"><img src=""></a>
+        <a class="rca_logo" href="#"><img src=""></a>
+      </div>
+      <div class="medium-6 large-6 columns title">
+        <div>
+          <h1>1001, rue Lenoir, Montréal</h1>
+          <span>Text here</span>
+        </div>
+      </div>
+      <div class="medium-3 large-3 columns navig">
+        <a class="info_btn" href="#"><img src="img/icon_info.png"></a>
+      </div>
+    </div>
+    
+     
+    
+    
+    <div class="row">    
+      <div class="large-12">
+        <!-- <div class="row">
+          <div class="topbanner">
+            <img src="img/adbanner_728x90.png" alt="Ad banner">
+          </div>
+        </div> -->
+        <div class="underspotlight">
+          <img src="img/temp_caroussel.jpg">
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="medium-4 large-4 columns leftcontent">  
+        <div class="announcements">
+          <h2>Announcements<!-- <small>Page subtitle</small> --></h2>
+
+          
+          <div class="announcement_item">
+            <a href="#"><h3>Title 1<small>[RCA]</small></h3></a>
+            <span class="announcement_date">12/12/14</span>              
+            <p class="announcement_text">Bacon ipsum dolor sit amet salami ham hock biltong ball tip drumstick sirloin pancetta meatball short loin. Venison tail chuck pork chop, andouille ball tip beef ribs flank boudin bacon. Salami andouille pork belly short ribs flank cow. Salami sirloin turkey kielbasa. Sausage venison pork loin leberkas chuck short loin, cow ham prosciutto pastrami jowl. Ham hock jerky tri-tip, fatback hamburger shoulder swine pancetta ground round. Tri-tip prosciutto meatball turkey, brisket spare ribs shankle chuck cow chicken ham hock boudin meatloaf jowl.</p>
+            <a class="readmore_link" href="#">Read more ></a>
+          </div>
+
+          <div class="announcement_item">
+            <a href="#"><h3>Title 2</h3></a>
+            <span class="announcement_date">12/12/14</span>              
+            <p class="announcement_text">Ground round pastrami pork loin tenderloin jerky. Jerky spare ribs biltong, ham hock ham capicola pork. Jerky turducken pork, meatloaf sausage capicola swine corned beef turkey short loin. Tongue prosciutto pork loin, ground round spare ribs venison kielbasa strip steak.</p>
+            <a class="readmore_link" href="#">Read more ></a>
+          </div>
+
+          <div class="announcement_item">
+            <a href="#"><h3>Title 3</h3></a>
+            <span class="announcement_date">12/12/14</span>              
+            <p class="announcement_text">Hamburger bresaola turkey t-bone, leberkas salami pork chop ham hock beef ribs. Rump biltong meatball venison, short ribs pork loin shank shankle corned beef beef. Cow salami jowl short loin hamburger fatback. Short ribs pork belly shoulder pastrami drumstick salami corned beef ham hock bresaola. Swine filet mignon cow sausage ball tip. Cow ribeye ground round, sausage pork loin pig beef ball tip turkey boudin.</p>
+            <a class="readmore_link" href="#">Read more ></a>
+          </div>
+
+          <a class="announcement_archive_link" href="#">View archive >></a>
+        </div>
+      </div>
+
+      <div class="medium-8 large-8 columns rightcontent">
+        <div class="events">
+          <img src="img/temp_caroussel.jpg">
+        </div>
+        <div class="boxes_section">
+          <select name="category">
+            <option value="" disabled="disabled" selected="selected">Select category</option>
+            <option value="artist">Artist</option>
+            <option value="finance">Finance</option>
+            <option value="foodbeverage">Food & Beverage</option>
+            <option value="media">Media</option>
+            <option value="other">Other</option>
+            <option value="retail">Retail</option>
+            <option value="technology">Technology</option>
+            <option value="travelleisure">Travel & Leisure</option>
+          </select>
+          <!-- <input type="search" name="searchtenant"><input type="submit"> -->
+
+                                              <!--       MEMBERS LIST       -->
+          <ul class="boxes_list">
+
+            <!-- START COPY HERE -->
+            <div class="small-6 medium-4 large-4 columns">
+              <li class="box_item">
+                <a><div>
+                  <span class="helper"></span><img class="box_logo" src="members/neoinformatics/img/neoinformatics_0.png">
+                  <div class="caption"><h3 class="business_name">Business name</h3><span class="location">Suite B-521</span></div>
+                </div></a>
+              </li>
+            </div>
+            <!-- END HERE -->
+
+            <div class="small-6 medium-4 large-4 columns">
+              <li class="box_item">
+                <a><div>
+                  <span class="helper"></span><img class="box_logo" src="members/neoinformatics/img/neoinformatics_0.png">
+                  <div class="caption"><h3 class="business_name">Business name 2</h3><span class="location">5645 Yonge Street, Toronto, ON, Canada, M2M 3T2</span></div>
+                </div></a>
+              </li>
+            </div>
+
+            <div class="small-6 medium-4 large-4 columns">
+              <li class="box_item">
+                <a><div>
+                  <span class="helper"></span><img class="box_logo" src="members/neoinformatics/img/neoinformatics_0.png">
+                  <div class="caption"><h3 class="business_name">Business name 3</h3><span class="location">Location</span></div>
+                </div></a>
+              </li>
+            </div>
+
+            <div class="small-6 medium-4 large-4 columns">
+              <li class="box_item">
+                <a><div>
+                  <span class="helper"></span><img class="box_logo" src="members/neoinformatics/img/neoinformatics_0.png">
+                  <div class="caption"><h3 class="business_name">Business name 4</h3><span class="location">Location</span></div>
+                </div></a>
+              </li>
+            </div>
+
+            <div class="small-6 medium-4 large-4 columns">
+              <li class="box_item">
+                <a><div>
+                  <span class="helper"></span><img class="box_logo" src="members/neoinformatics/img/neoinformatics_0.png">
+                  <div class="caption"><h3 class="business_name">Business name 5</h3><span class="location">Location</span></div>
+                </div></a>
+              </li>
+            </div>
+
+            <div class="small-6 medium-4 large-4 columns">
+              <li class="box_item">
+                <a><div>
+                  <span class="helper"></span><img class="box_logo" src="members/neoinformatics/img/neoinformatics_0.png">
+                  <div class="caption"><h3 class="business_name">Business name 6</h3><span class="location">Location</span></div>
+                </div></a>
+              </li>
+            </div>
+
+            <div class="small-6 medium-4 large-4 columns">
+              <li class="box_item">
+                <a><div>
+                  <span class="helper"></span><img class="box_logo" src="members/neoinformatics/img/neoinformatics_0.png">
+                  <div class="caption"><h3 class="business_name">Business name 7</h3><span class="location">Location</span></div>
+                </div></a>
+              </li>
+            </div>
+
+            <div class="small-6 medium-4 large-4 columns end">
+              <li class="box_item">
+                <a><div>
+                  <span class="helper"></span><img class="box_logo" src="members/neoinformatics/img/neoinformatics_0.png">
+                  <div class="caption"><h3 class="business_name">Business name 8</h3><span class="location">Location</span></div>
+                </div></a>
+              </li>
+            </div>
+
+            <!-- LAST DIV MUST HAVE "end" as class name -->
+
+          </ul>
+          <div class="small-12 large-12 columns viewall"><a class="button" href="#">View all</a></div>
+        </div>
+        
+      </div>
+    </div>
+        
+          
+
+      
+    <div class="footer row">
+      <div class="inner">
+        <div class="large-6 columns footer_1mile">
           <a class="1mile_logo" href="#"><img src=""></a>
+          <ul>
+            <li><i class="foundicon-location"></i>1001, Rue Lenoir, Montréal, Québec, Canada, H4C 2Z6</li>
+            <li><i class="foundicon-phone"></i>514-989-8780</li>
+            <li><i class="foundicon-mail"></i>lorem@ipsum.com</li>
+          </ul>
+        </div>
+        <div class="large-6 columns footer_rca">
           <a class="rca_logo" href="#"><img src=""></a>
-        </div>
-        <div class="large-6 columns title">
-          <div>
-            <h1>1001, rue Lenoir, Montréal</h1>
-            <span>Text here</span>
-          </div>
-        </div>
-        <div class="large-3 columns navig">
-          <a class="info_btn" href="#"><img src="img/icon_info.png"></a>
+          <ul>
+            <li><i class="foundicon-location"></i>Edifice RCA 1001, Rue Lenoir, suite A-202, Montréal, Québec, H4C 2Z6</li>
+            <li><i class="foundicon-phone"></i>514-933-2211</li>
+            <li><i class="foundicon-mail"></i>lorem@ipsum.com</li>
+          </ul>
         </div>
       </div>
-      
-       
-      
-      
-      <div class="row">    
-        
-         
-         
-        <div class="large-10 columns">
-          <div class="row">
-            <div class="topbanner">
-              <img src="img/adbanner_728x90.png" alt="Ad banner">
-            </div>
-          </div>
-
-          <div class="row content">
-            <div class="large-5 columns leftcontent">
-              <div class="row">
-                <img src="img/temp_caroussel.jpg">
-              </div>
-
-              <h3>Page Title <small>Page subtitle</small></h3>
-            
-              <p>Bacon ipsum dolor sit amet salami ham hock biltong ball tip drumstick sirloin pancetta meatball short loin. Venison tail chuck pork chop, andouille ball tip beef ribs flank boudin bacon. Salami andouille pork belly short ribs flank cow. Salami sirloin turkey kielbasa. Sausage venison pork loin leberkas chuck short loin, cow ham prosciutto pastrami jowl. Ham hock jerky tri-tip, fatback hamburger shoulder swine pancetta ground round. Tri-tip prosciutto meatball turkey, brisket spare ribs shankle chuck cow chicken ham hock boudin meatloaf jowl.</p>
-         
-              <p>Ground round pastrami pork loin tenderloin jerky. Jerky spare ribs biltong, ham hock ham capicola pork. Jerky turducken pork, meatloaf sausage capicola swine corned beef turkey short loin. Tongue prosciutto pork loin, ground round spare ribs venison kielbasa strip steak.</p>
-         
-              <p>Hamburger bresaola turkey t-bone, leberkas salami pork chop ham hock beef ribs. Rump biltong meatball venison, short ribs pork loin shank shankle corned beef beef. Cow salami jowl short loin hamburger fatback. Short ribs pork belly shoulder pastrami drumstick salami corned beef ham hock bresaola. Swine filet mignon cow sausage ball tip. Cow ribeye ground round, sausage pork loin pig beef ball tip turkey boudin.</p>
-         
-              <p>Prosciutto ball tip filet mignon andouille frankfurter chicken rump sausage meatball. Filet mignon meatloaf ground round andouille ham hock pork. Bresaola short loin meatball chuck hamburger pig. Turkey venison chuck, tongue fatback tail swine jerky corned beef shank kielbasa prosciutto ribeye ham tri-tip. Rump bacon pork belly meatloaf shoulder short loin meatball kielbasa pork loin tongue bresaola brisket corned beef jowl prosciutto. Beef ribs shankle short ribs pork belly corned beef fatback pork chop tongue biltong boudin strip steak sirloin meatloaf pancetta.</p>
-
-            </div>
-
-            <div class="large-7 columns rightcontent">
-              <h3>Page Title <small>Page subtitle</small></h3>
-            
-              <p>Bacon ipsum dolor sit amet salami ham hock biltong ball tip drumstick sirloin pancetta meatball short loin. Venison tail chuck pork chop, andouille ball tip beef ribs flank boudin bacon. Salami andouille pork belly short ribs flank cow. Salami sirloin turkey kielbasa. Sausage venison pork loin leberkas chuck short loin, cow ham prosciutto pastrami jowl. Ham hock jerky tri-tip, fatback hamburger shoulder swine pancetta ground round. Tri-tip prosciutto meatball turkey, brisket spare ribs shankle chuck cow chicken ham hock boudin meatloaf jowl.</p>
-         
-              <p>Ground round pastrami pork loin tenderloin jerky. Jerky spare ribs biltong, ham hock ham capicola pork. Jerky turducken pork, meatloaf sausage capicola swine corned beef turkey short loin. Tongue prosciutto pork loin, ground round spare ribs venison kielbasa strip steak.</p>
-         
-              <p>Hamburger bresaola turkey t-bone, leberkas salami pork chop ham hock beef ribs. Rump biltong meatball venison, short ribs pork loin shank shankle corned beef beef. Cow salami jowl short loin hamburger fatback. Short ribs pork belly shoulder pastrami drumstick salami corned beef ham hock bresaola. Swine filet mignon cow sausage ball tip. Cow ribeye ground round, sausage pork loin pig beef ball tip turkey boudin.</p>
-         
-              <p>Prosciutto ball tip filet mignon andouille frankfurter chicken rump sausage meatball. Filet mignon meatloaf ground round andouille ham hock pork. Bresaola short loin meatball chuck hamburger pig. Turkey venison chuck, tongue fatback tail swine jerky corned beef shank kielbasa prosciutto ribeye ham tri-tip. Rump bacon pork belly meatloaf shoulder short loin meatball kielbasa pork loin tongue bresaola brisket corned beef jowl prosciutto. Beef ribs shankle short ribs pork belly corned beef fatback pork chop tongue biltong boudin strip steak sirloin meatloaf pancetta.</p>
-            </div>
-          </div>
-          
-                
-        </div>
-        
-        
-         
-         
-        <div class="large-2 columns sidepanel">
-          <div class="toppanel">
-            <a class="button" href="#">Classifieds</a>
-          </div>
-          
-          <div class="secondbanner">
-            <p><img src="img/adbanner_300x600.png"/></p>
-          </div>
-            
-        </div>
-        
-      </div>
+    </div>
         
       
        
